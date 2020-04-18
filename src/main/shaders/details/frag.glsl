d7a54ca954d4758aa4ff5ae99e20ab8a0a5802aa
Andrew Johnson <ajzat25@gmail.com>
2020-04-18 18:40:33 -0400

    fixed loop out of bounds

--- src/main/shaders/details/frag.glsl
+++ src/main/shaders/details/frag.glsl
@@ -164,7 +164,7 @@ highp float edgedetect(ivec2 t) {
   // calculate the standard deviation
   highp float acc = 0.0;
   highp float diff = 0.0;
-  for (mediump int i = 0; i <= 13; i++) {
+  for (mediump int i = 0; i < 13; i++) {
     diff = p[i]-mean;
     acc += diff*diff;
   }
